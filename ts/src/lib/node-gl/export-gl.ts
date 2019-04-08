fcefe1392587ce5495a64969a0f8c7d403fef44f
Charles Samborski <demurgos@demurgos.net>
2019-04-08 13:26:25 +0200

    [ts] Remove `node-gl` prototype

--- ts/src/lib/node-gl/export-gl.ts
+++
@@ -1,7 +0,0 @@
-export function exportGl(ctx: WebGLRenderingContext): ImageData {
-  const width: number = ctx.drawingBufferWidth;
-  const height: number = ctx.drawingBufferHeight;
-  const data: Uint8Array = new Uint8Array(width * height * 4);
-  ctx.readPixels(0, 0, width, height, ctx.RGBA, ctx.UNSIGNED_BYTE, data);
-  return {width, height, data: Uint8ClampedArray.from(data)};
-}
